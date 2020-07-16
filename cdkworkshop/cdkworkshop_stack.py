a3907071b0b7c0dfd37b0a4a1e69d8bc5825e91a
Katsunori Takahashi <katsunori.takahashi.eng@gmail.com>
2020-07-16 22:19:46 +0900

    Complete CDK workspace

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,8 +1,12 @@
 from aws_cdk import (
+    core,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
-    core,
 )
+
+from cdk_dynamo_table_viewer import TableViewer
+from hitcounter import HitCounter
+
 
 class CdkworkshopStack(core.Stack):
 
@@ -16,7 +20,18 @@ class CdkworkshopStack(core.Stack):
             handler='hello.handler',
         )
 
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
+        )
+
         apigw.LambdaRestApi(
             self, 'Endpoint',
-            handler=my_lambda,
+            handler=hello_with_counter.handler,
+        )
+
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
         )
--- lambda/hello.py
+++ lambda/hello.py
@@ -1,5 +1,6 @@
 import json
 
+
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
     return {
