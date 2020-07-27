9858820637f6b1fea43a4463b10c37e5b84db306
Katsunori Takahashi <katsunori.takahashi.eng@gmail.com>
2020-07-27 20:16:14 +0900

    add config file

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from aws_cdk import core
 
 from cdkworkshop.cdkworkshop_stack import CdkworkshopStack
 
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 app = core.App()
-CdkworkshopStack(app, "cdkworkshop", env={'region': 'ap-northeast-1'})
+CdkworkshopStack(app, "cdkworkshop", env={'region': config['SETUP']['region']})
 
 app.synth()
